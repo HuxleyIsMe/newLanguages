810726cee0e402481119fee7590fca5de9075166
hux <huxley.millard@gmail.com>
2025-05-07 15:58:11 +0200

    feat: i am having more fun with this, we've worked a little about bash an how fastidious it is. But it makes sense the letters aren't random although the brackets are a situation

--- day_2_bash/src/project.sh
+++ day_2_bash/src/project.sh
@@ -1,8 +1,21 @@
-#!/bin/zsh
-
 #!/usr/bin/env bash
 
 echo "Welcome to our project!"
 
-echo "NOT IMPLEMENTED!" >&2
-exit 1
+
+read -p "What is your name? " name
+
+read -p "What year were you born? " yearOfBirth
+
+age=$((2024 - $yearOfBirth))
+
+if [ $age -lt 30 ]
+then
+echo 'ooo still sneakily in your twenties!'
+elif [ $age -gt 30 ]; then
+echo 'welcome to the squeaky knees club'
+else
+echo 'how did you trigger this?'
+fi
+
+echo "$name $age"
